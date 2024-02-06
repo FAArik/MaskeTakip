746e8fdaadf0df41094ca2827a3177d02e9ee2ad
FAArik <62200015+FAArik@users.noreply.github.com>
2024-02-06 12:24:57 +0300

    Add project files.

---
+++ MaskeTakip/Program.cs
@@ -0,0 +1,26 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+namespace MaskeTakip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "Fatih Ali";
+            person1.LastName = "Arık";
+            person1.DateOfBirthYear = 2000;
+            person1.NationalIdentity = 12312312312;//placeholder
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+           
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
